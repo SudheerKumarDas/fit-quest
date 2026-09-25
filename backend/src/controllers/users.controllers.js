2fe4cebe73011f024dcf2a7ed1a8f96dd190b8d7
Sudheer Kumar Das <samratsudheerdas@gmail.com>
2026-09-25 15:53:07 +0545

    user logged and generate jwt token and save to cookie successfully

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,34 +1,81 @@
 import User from "../models/users.models.js";
 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const usersRegister = async (req,res) => {
-    try {
-        const { username, email, password } = req.body;
-        if(!username || !email || !password){
-            return res.status(400).json({
-                message:"provide all fields"
-            })
-        }
-        const existingUser = await User.findOne({email})
-        if(existingUser){
-            return res.status(409).json({
-                message:"user already exists"
-            })
-        }
-        const hashedPassword = await bcrypt.hash(password,10);
-        const newUser = await User.create({
-            username:username,
-            email:email,
-            password:hashedPassword
-        })
-        res.status(201).json({
-            message:"New User Created Successfully"
-        })
-    } catch (error) {
-        console.error("Error in user register ",error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
+export const usersRegister = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "provide all fields",
+      });
     }
-}
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        message: "user already exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      message: "New User Created Successfully",
+    });
+  } catch (error) {
+    console.error("Error in user register ", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const usersLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "provide all fields",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "provide valid credentials",
+      });
+    }
+    const isPasswordMatch = bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(403).json({
+        message: "provide valid credentials",
+      });
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+    res.cookie("token",token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000,
+    })
+    res.status(200).json({
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    console.error("error in user login");
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { usersRegister } from "../controllers/users.controllers.js";
+import { usersLogin, usersRegister } from "../controllers/users.controllers.js";
 
 const router = express.Router();
 
 router.post("/register",usersRegister);
+router.post("/login",usersLogin);
 
 export default router;
